92c1487d37c895a733eee687aebc5a5f721b004b
dawerna <50948563+dawerna@users.noreply.github.com>
2019-07-11 12:23:40 +0200

    Add files via upload

--- Codeblocks V(werner)1.0/DateiAuslesenPrototyp.java
+++ Codeblocks V(werner)1.0/DateiAuslesenPrototyp.java
@@ -1,31 +1,32 @@
 import java.io.*;
+import java.lang.*;
 
 public class DateiAuslesenPrototyp
 {
     
-    private DateiAuslesenPrototyp dap = new DateiauslesenPrototyp();
+    
     
     public DateiAuslesenPrototyp()
     { 
         
-        //--°
+        
       }
       
-    public void dateiLesen(){
+    public String dateiLesen(){
         try{
-            FileReader fr = new FileReader("baum.txt");
+            FileReader fr = new FileReader("config.txt");
             BufferedReader br = new BufferedReader(fr);
             //FileReader liest aus der Datei, BufferedReader liest Daten des FileReader
             //aus
-            String zeile = "";
-            //Liest eine Zeile aus
+            String zeile = br.readLine();
             
-            while( (zeile = br.readLine()) != null){
-              System.out.println(zeile);
-              //Vorgang wird bei vorhandenen Zeilen wiederholt
-            }
-            
+           // while( (zeile = br.readLine()) != null){
+             // System.out.println(zeile);
+             //Liest nacheinander alle Zeilen aus
+           // }
             br.close();
+            return zeile;
+            
             //BufferedReader wird geschlossen
         }
         
@@ -33,22 +34,75 @@ public class DateiAuslesenPrototyp
             System.out.println("Datei konnte nicht ausgelesen werden."+ e.getMessage());
             //Fehlermeldung
         }
+        return "Attempt failed";
+        
+    }
+   
+    private String readConfig(){
+        try{
+            FileReader fr = new FileReader("config.txt");
+            BufferedReader br = new BufferedReader(fr);
+            
+            //FileReader liest aus der Datei, BufferedReader liest Daten des FileReader
+            //aus
+            
+            String zeile = br.readLine();
+              
+            br.close();
+            return zeile;
+            
+        }
+        
+        catch(IOException e){
+            System.out.println("Datei konnte nicht ausgelesen werden."+ e.getMessage());
+            //Fehlermeldung
+        }
+        return "Attempt failed";
+        
+    }
+    
+    public void adjustNewSettings(){
+        //Soll die Angaben aus der txt-Dokument die config auslesen und anpassen
+        String config = dateiLesen();
+        
+        
+        String[] parts = config.split(": ");
+        String part1 = parts[0];
+        String part2 = parts[1];
+        
+        switch(config){
+         case "theme":
+         //falls erstes Statement theme ist: 
+         //bsp.: (theme: dark)
+         if(true){
+             //theme auf dark setzen 
+            }
+         else{
+             //theme auf light setzen
+            }
+         break;   
+         
+         case "width":
+         int width = Integer.parseInt(parts[1]);
+         // Fensterbreite anpassen
+            
+         break;
+         
+         //case "language":
+         //break;
+        }
         
         
     }
     
-    public void readNewSettings(){
-        
-        dap.dateiLesen();
-        
-        
-    }
     
     public void DateiSchreiben(String sttngs){
         try{
             PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("baum.txt")));
             
-            pw.println(sttngs);
+            pw.println("theme: " );
+            pw.println("width: " );
+            // pw.println("langauge: " );
             
             pw.close();
         }
@@ -58,5 +112,6 @@ public class DateiAuslesenPrototyp
         }
         
     }
+    
     
 }
--- Codeblocks V(werner)1.0/Settings.java
+++ Codeblocks V(werner)1.0/Settings.java
@@ -1,16 +1,20 @@
-import java.io.FileReader;
+import java.lang.*;
 import java.io.*;
+
+
 /**
- * Write a description of class Settings here.
+ * Einstellungsklasse: verwaltet theme (dark/light), Fensterbreite (x) und evtl. Sprache
  *
  * @author Simon Werner
- * @version 04.07.2019
+ * @version 11.07.2019
  */
 public class Settings
 {
+    
     private boolean theme; 
     private Language language;
     private int sidebarLength;
+    
     public Settings(boolean theme, int sidebarLength)
     {
       }
@@ -25,36 +29,102 @@ public class Settings
         //gibt aktuelles Theme heraus
     }
     
+    public void setThemeLight(){
+        if(getTheme() != true){
+        this.theme = true;
+        //ändert aktuelles theme zu light
+        }else
+       {
+        }
+    }
+    
+    public void setThemeDark(){
+        if(getTheme() != false){
+         this.theme = false;
+        //ändert aktuelles theme zu dark
+        }else
+       {
+        }
+    }
+    
     public int sidebarLength(){
         return sidebarLength;
         //gibt aktuelle Seitenlänge in Pixel heraus
     }
     
+    
     public void loadFromConfig(){
-        try{
-            FileReader fr = new FileReader("baum.txt");
+         try{
+            FileReader fr = new FileReader("config.txt");
             BufferedReader br = new BufferedReader(fr);
             
-            //FileReader liest aus der Datei, BufferedReader liest Daten des FileReader
-            //aus
-            while(true){
-              String zeile = br.readLine();
-              System.out.println(zeile);
-              //  
+            //FileReader liest aus der Datei "config.txt", BufferedReader liest Daten des
+            //FileReaders aus
+            
+            String line = "";
+            
+            while( (line = br.readLine()) != null){
+                //Solange es Zeilen zum Auslesen gibt, wird weiter gelesen
+              line = br.readLine();
+              
+              String[] parts = line.split(": ");
+              String part1 = parts[0];
+              String part2 = parts[1];
+        
+               switch(line){
+                  case "theme":
+                  
+                  if(parts[1]=="dark"){
+                      setThemeDark(); 
+                      //theme auf dark setzen 
+                    }
+                  else{
+                      setThemeLight();
+                      //theme auf light setzen
+                }
+                break;   
+         
+                case "sidebarLength":
+                int x = Integer.parseInt(parts[1]);
+                this.sidebarLength = x;
+                // Fensterbreite anpassen
+                break;
+         
+                //case "language":
+                //break;
+              }
+              
             }
-        }
+            System.out.println("finished");
+            br.close();
+            
+           }
         
         catch(IOException e){
             System.out.println("Datei konnte nicht ausgelesen werden."+ e.getMessage());
             //Fehlermeldung
-        }
+       }
         
         
     }
     
-    public void setFromConfig(){
+    public void setConfig(String themetype, String sidebarLength){
+   
+        try{
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("config.txt")));
+            
+            pw.println("theme: " + themetype);
+            pw.println("sidebarLength: " + sidebarLength);
+            // pw.println("langauge: " );
+            
+            pw.close();
+        }
+        catch(IOException io){
+            System.out.println("Datei konnte nicht bearbeitet werden" + io.getMessage());
+            //Fehlermeldung
+        }
+        
     
-        //
     }
     
 }
